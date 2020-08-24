489165a48d1bdb39141f7651594a00679478908c
Anthony Bergen <anthonydbergen@gmail.com>
2020-08-24 10:27:14 -0400

    added route files and connected routes to server

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // sets initial port
 var PORT = process.env.PORT || 8080;
 
+// routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // listener to start server
 app.listen(PORT, function () {
